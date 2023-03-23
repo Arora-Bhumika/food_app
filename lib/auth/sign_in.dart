6cd76ea3c45881d47b87ab1844a51a4174245f6c
Bhumika Arora <bhumikaarora1905@gmail.com>
2023-03-23 15:47:49 +0530

    Create sign_in.dart

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets\login_background.png')),
+        ),
+      ),
+    );
+  }
+}
